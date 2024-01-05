641ce5fa0c68790bded3ba70158d4af09a38b549
Alexander Urbina mar <aurbina@hiper.com.pe>
2024-01-05 00:14:34 -0500

    add command for keys

---
+++ products-service/infrastructure/src/main/java/com/mvnnixbuyapi/keyProduct/rest/controller/KeyProductCommandController.java
@@ -0,0 +1,9 @@
+package com.mvnnixbuyapi.keyProduct.rest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/command-key-endpoint")
+public class KeyProductCommandController {
+}
