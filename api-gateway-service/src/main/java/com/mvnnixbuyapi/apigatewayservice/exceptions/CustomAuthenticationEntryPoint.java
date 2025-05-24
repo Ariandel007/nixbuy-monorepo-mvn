d004a9b8649efe306b23dfac436cf3830290c88b
Alexander Urbina mar <aurbina@hiper.com.pe>
2025-05-24 00:44:58 -0500

    add jaeger configuration AND assimetrical jwt validation

---
+++ api-gateway-service/src/main/java/com/mvnnixbuyapi/apigatewayservice/exceptions/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package com.mvnnixbuyapi.apigatewayservice.exceptions;
+
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class CustomAuthenticationEntryPoint implements ServerAuthenticationEntryPoint {
+
+    @Override
+    public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException ex) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED); // 401
+        exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+
+        String body = """
+                {
+                    "status": 401,
+                    "error": "Unauthorized",
+                    "message": "Token is missing or invalid"
+                }
+                """;
+
+        DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(body.getBytes(StandardCharsets.UTF_8));
+        return exchange.getResponse().writeWith(Mono.just(buffer));
+    }
+}
